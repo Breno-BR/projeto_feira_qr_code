9d8a1b6ef5a3a5072309bfc97dff084302f58c42
Breno Belém Martins de Albuquerque <62098988+Breno-BR@users.noreply.github.com>
2021-12-03 02:03:14 -0300

    versão final

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from functions import *
 
+
 col1, col2, col3 = st.columns([1, 6, 1])
 
 with col1:
@@ -19,15 +20,39 @@ with col3:
 st.title("DEPED - Locker")
 '''\n'''
 
+
 st.subheader("Objetos cadastrados")
-st.table(sql_load_data('dados.db'))
+'''\n'''
+df = sql_load_data()
+
+imagem = 'blank.jpg'
+
+for i in range(0, len(df)):
+    col1, col2, col3, col4 = st.columns(4)
+    with st.container():
+        with col1:
+            st.write(df.iloc[i, 0])
+        with col2:
+            if st.button("QR Code", key=f'{df.iloc[i, 0]}'):
+                gerar_codigo(df.iloc[i, 0])
+                imagem = f'{df.iloc[i, 0]}.png'
+        with col3:
+            st.write(df.iloc[i, 1])
+        with col4:
+            if st.button("Delete", key=f'{df.iloc[i, 0]}'):
+                sql_del_data(df.iloc[i, 0])
+                remove_img(df.iloc[i, 0])
+                print(df.iloc[i, 0])
 
 st.sidebar.subheader("Enviar arquivo LOEC")
 arquivo = st.sidebar.file_uploader("Enviar o arquivo com os códigos dos objetos:")
 if arquivo is not None:
     arquivo_carregado = carregar_arquivo(arquivo)
     arquivo_carregado.iloc[:, 0].apply(sql_save_data)
-    # st.table(arquivo_carregado.head())
     submitted = st.sidebar.button("Gravar LOEC")
     if submitted:
         st.sidebar.write("LOEC gravada!")
+
+if imagem != 'blank.jpg':
+    st.sidebar.header(imagem.strip(".png"))
+    st.sidebar.image(imagem)
--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
-import pandas as pd
+import os
 from pandas import read_excel, read_csv, DataFrame
 import sqlite3
 from datetime import datetime as dt
-# import qrcode
+import qrcode
+from PIL import Image
+
 
 def carregar_arquivo(file):
 
@@ -28,9 +30,9 @@ def sql_save_data(codigo_obj, data_atual=dt.now().strftime("%d/%m/%Y")):
         print("Error while connecting to sqlite", error)
 
 
-def sql_load_data(db):
+def sql_load_data():
     try:
-        con = sqlite3.connect(db)
+        con = sqlite3.connect('dados.db')
         cur = con.cursor()
         print("Conexão realizada com sucesso!")
         qry = "SELECT * FROM loec"
@@ -38,27 +40,38 @@ def sql_load_data(db):
         rows = cur.fetchall()
         df = DataFrame(rows)
         df.columns = ['Objeto', 'Data']
+        df.drop_duplicates(subset='Objeto', keep='first', inplace=True)
         con.close()
         return df
     except sqlite3.Error as error:
         print("Error while connecting to sqlite", error)
 
 
+def gerar_codigo(codigo):
+    qr = qrcode.QRCode()
+    qr.add_data(codigo)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    qr_res = img.resize((512, 512), Image.NORMAL)
+    qr_res.save(str(codigo)+'.png')
 
 
-#
-# df = carregar_arquivo('Códigos.xlsx')
-#
-# sql_load_data('dados.db')
-#
-#
-# len(df.iloc[0, 0])
-#
-#
-# len(dt.now().strftime("%d/%m/%Y"))
-#
-# arq = df.iloc[0, 0]
-# data = dt.now().strftime("%d/%m/%Y")
-#
-# sql_save_data(arq)
+def sql_del_data(codigo):
+    try:
+        con = sqlite3.connect('dados.db')
+        cur = con.cursor()
+        print("Conexão realizada com sucesso!")
+        qry = "DELETE FROM loec WHERE codigo=?"
+        cur.execute(qry, (codigo,))
+        con.commit()
+        con.close()
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+
+
+def remove_img(codigo):
+    imgs = [i for i in os.listdir(".") if i.endswith(".png")]
+    check = any([i.startswith(codigo) for i in imgs])
+    if check:
+        os.remove(f'{codigo}.png')
 
